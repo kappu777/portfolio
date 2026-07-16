70655ad7b0c11daebabfcb02e1ab9ed25c2272d3
Kapil Singhal <kapsingh5@publicisgroupe.net>
2026-07-15 21:14:20 -0700

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-712ab3578f8f83af0a8f.js"
+    "url": "webpack-runtime-225aff37f623a58beefc.js"
   },
   {
     "url": "framework-b589eed34fa00dc27413.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "548d99719d047a0e6e655e72e1f9be08"
+    "revision": "f8f8a71e3568b5723af1ba5f77a0a6b4"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-162d54bec23a104748fe.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "b347debe9032a5401062f76b2523ec1f"
+    "revision": "89244e845328b3a7654cfa137641bd1d"
   },
   {
     "url": "manifest.webmanifest",
